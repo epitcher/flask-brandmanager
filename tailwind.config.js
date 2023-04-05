229aa077ca35b5ff5cf338b9bf7e6c4ba33cfdcf
Edward Pitcher <epitcherbridel@gmail.com>
2023-04-05 08:57:40 +0100

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './app/modules/consumer/views/**/*.html',
+        './app/modules/author/views/**/*.html',
+    ],
+    theme: {
+        extend: {}
+    },
+    variants: {},
+    plugins: []
+}
