2e612dd2eccec744636d716681e6d073e3a41a06
Edward Pitcher <epitcherbridel@gmail.com>
2023-04-06 10:37:49 +0100

    Remove main.py

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify, render_template
-
-main = Blueprint('main', __name__)
-
-@main.route('/')
-def home():
-    return render_template('index.html')
-
-# ... (other routes)
