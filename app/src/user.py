5c46cc90cc57cb8ee7869589d4d29ac5a2b2b3b0
Edward Pitcher <epitcherbridel@gmail.com>
2023-04-06 12:17:33 +0100

    Update user.py`

--- app/src/user.py
+++ app/src/user.py
@@ -1,13 +1,21 @@
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 class User(UserMixin):
     def __init__(self, username, password, authenticated):
         self.id = username
         self.password = password
         self.authenticated = authenticated
 
+
 users = {
-    "user1": {"password": generate_password_hash("password1"), "authenticated": False},
-    "user2": {"password": generate_password_hash("password2"), "authenticated": False}
+    "user": {
+        "password": generate_password_hash("user"),
+        "authenticated": False
+    },
+    "admin": {
+        "password": generate_password_hash("password"),
+        "authenticated": False
+    }
 }
