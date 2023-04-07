349ca9af6181c61ca2996e6c5bbfb161f48eb798
Edward Pitcher <epitcherbridel@gmail.com>
2023-04-07 09:24:37 +0100

    Fix HTTP response codes on error pages

--- app/__init__.py
+++ app/__init__.py
@@ -56,10 +56,10 @@ def create_app():
 
     @app.errorhandler(404)
     def not_found_error(error):
-        return render_template('404.html')
-
+        return render_template('404.html'), 404
+    
     @app.errorhandler(500)
     def not_found_error(error):
-        return render_template('500.html')
+        return render_template('500.html'), 500
 
     return app
